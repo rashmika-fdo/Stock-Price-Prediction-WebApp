3db7c32ba141c2d78686088c8d6056321e5db3ca
Rashmika Fernando <lahfdo20@gmail.com>
2025-05-24 21:52:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from tensorflow.keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
+import subprocess
 
-model = load_model('Model3.keras', compile=False)
+if not os.path.isfile('model_new.keras'):
+    subprocess.run(['curl --output model_new.keras "https://github.com/rashmika-fdo/Stock-Price-Prediction-WebApp/blob/18632af440a3c2a6a6eef84624869f0af724782f/Model3.keras
+"'], shell=True)
+model = load_model('model_new.keras', compile=False)
 
 st.header('Stock Price Predictor')
 
